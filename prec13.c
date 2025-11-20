4be10dd75c5ced4d18bc050614ebb34321ef42d3
Utsav_Vara_ <varautsav73@gmail.com>
2025-11-20 22:09:06 +0530

    Create prec13.c

---
+++ prec13.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for(i=0;i<=100;i=i+10)
+    {
+        printf("Current Water Level: %d\n",i);
+    }
+    if (i=100)
+    {
+        printf("Tank is full\n");
+    }
+    printf("Name: utsav vara ID: 25CE134\n");
+
+    return 0;
+}
