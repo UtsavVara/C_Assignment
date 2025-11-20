e99372cd8bc132a28ea55941e491a4ccfc0d99dc
Utsav_Vara_ <varautsav73@gmail.com>
2025-11-20 22:11:35 +0530

    Update prec14.c

--- prec14.c
+++ prec14.c
@@ -3,16 +3,34 @@
 
 int main()
 {
-    int i;
-    for(i=0;i<=100;i=i+10)
+    int loop=1;
+    float balance=5000,withdraw;
+    while (loop==1)
     {
-        printf("Current Water Level: %d\n",i);
+        printf("Welcome to bank. Please enter amount of money you want to withdraw\n");
+        scanf("%f",&withdraw);
+        if (withdraw < 0)
+        {
+            printf("Nice try\n");
+        }
+        else if (withdraw <= balance)
+        {
+            balance = balance - withdraw ;
+            printf("Remaining balance is %f",balance);
+        }
+        else
+        {
+            printf("Insufficient balance, cannot withdraw\n");
+        }
+        printf("Do you want to withdraw more money? 1 for yes, 0 for no\n");
+        scanf("%d",&loop);
+        if (loop!=0 && loop!=1)
+        {
+            printf("Please use proper values, 0 or 1\n");
+            loop=0;
+        }
     }
-    if (i=100)
-    {
-        printf("Tank is full\n");
-    }
-    printf("Name: utsav vara ID: 25CE134\n");
+    printf("Name: utsav vara ID:25CE134 \n");
 
     return 0;
 }
