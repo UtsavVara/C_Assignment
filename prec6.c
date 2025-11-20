85c5931f4915d64ac8e2c4837e93f3b76044142d
Utsav_Vara_ <varautsav73@gmail.com>
2025-11-20 22:00:17 +0530

    Create prec6.c

---
+++ prec6.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+
+{
+    float celsius,farenheit;
+    printf("Please enter temperature in Celsius\n");
+    scanf("%f",&celsius);
+    farenheit=(celsius*9/5)+32;
+    printf("The temperature in farenheit is %f\n",farenheit);
+    printf("Name:utsav vara ID: 25CE134\n");
+
+    return 0;
+}
